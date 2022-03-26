4f3e783810fda5ad0a705f44ab3c2e2d8d1e6e7f
OlegBirykov <p_petro75@mail.ru>
2022-03-26 21:13:15 +0300

    Урок 12

--- src/store/info.js
+++ src/store/info.js
@@ -15,6 +15,18 @@ export default {
   },
 
   actions: {
+    async updateInfo({ dispatch, commit, getters }, toUpdate) {
+      try {
+        const uid = await dispatch("getUid");
+        const updateData = { ...getters.info, ...toUpdate };
+        await firebase.database().ref(`users/${uid}/info`).update(updateData);
+        commit("setInfo", updateData);
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
+
     async fetchInfo({ dispatch, commit }) {
       try {
         const uid = await dispatch("getUid");
@@ -23,7 +35,8 @@ export default {
         ).val();
         commit("setInfo", info);
       } catch (e) {
-        console.log(e);
+        commit("setError", e);
+        throw e;
       }
     },
   },
